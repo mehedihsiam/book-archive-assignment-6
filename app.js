f0297b967239d89d4190c9a922a54b59c88a4574
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-09-02 01:28:59 +0600

    added books dynamicly

--- app.js
+++ app.js
@@ -2,16 +2,46 @@ const loadBooks = () => {
     const searchInput = document.getElementById('saerch-input');
     const searchText = searchInput.value;
     searchInput.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data))
 }
 // loadBooks();
 
-const displayBooks = (books) => {
+const displayBooks = (booksContainer) => {
     const commonContainer = document.getElementById('common-container');
-    commonContainer.innerHTML = `
-    <small>Total <span class="text-danger">${books.numFound}</span> results found</small>
+
+    // result total print
+    const totalResult = document.createElement('div');
+    totalResult.innerHTML = `
+    <small>Total <span class="text-danger">${booksContainer.numFound}</span> results found</small>
     `
+    commonContainer.appendChild(totalResult);
+
+    // find books object
+    const books = booksContainer.docs;
+
+    // create book container
+    const bookContainer = document.createElement('div');
+    bookContainer.classList.add('row');
+    //loop on books
+    books.forEach(book => {
+        const bookBox = document.createElement('div');
+        bookBox.classList.add('text-center', 'col-md-4', 'g-5');
+        bookBox.innerHTML = `
+        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid">
+        <h5 class="text-center mt-3">${book.title.slice(0, 25)}</h5>
+        <p class="text-danger">Author: ${book.author_name}</p>
+        <p>Publisher: ${book.publisher_facet}</p>
+        <small class="d-block">First Published: ${book.first_publish_year}</small>
+        `
+        bookContainer.appendChild(bookBox);
+    });
+    commonContainer.appendChild(bookContainer);
+
+
+
+    // result print
+
 }
